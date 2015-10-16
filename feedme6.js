2f3ae5224abafd20c5b96c550483411cc1707145
Yassal <yassal@yds.se>
2015-10-16 12:06:47 +0200

    add the mobile menu

--- feedme6.js
+++ feedme6.js
@@ -1,4 +1,12 @@
-if (Meteor.isServer) {
+if (Meteor.isClient) {
+  Meteor.startup(() => {
+    console.log("collapse button set", $(".button-collapse"));
+    $(".button-collapse").sideNav();
+    console.log("collapse button set");
+  });
+}
+
+  if (Meteor.isServer) {
   Meteor.startup(() => {
     console.log('Starting feedme6');
   });
